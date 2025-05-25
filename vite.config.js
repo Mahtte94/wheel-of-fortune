e1cb46804c1d9e84b8870be45f68b60998b835f9
Mahtte94 <mahtias.jebrand@live.se>
2025-05-25 20:09:55 +0200

    added another vite global

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
   define: {
     __APP_ENV__: process.env.VITE_VERCEL_ENV,
   },
@@ -15,4 +16,5 @@ export default defineConfig({
       }
     }
   }
+}
 })
