96c570c519b91431e580ba9e1f04b782b9db1b3f
Mahtte94 <mahtias.jebrand@live.se>
2025-05-25 20:12:10 +0200

    change env definition

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
   define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    __APP_ENV__: JSON.stringify(env.APP_ENV),
   },
   plugins: [react()],
   server: {
