6b55440f4cce24bc9d1bbe0deb1ccad383a76cd1
Mahtte94 <mahtias.jebrand@live.se>
2025-05-25 20:06:27 +0200

    added vite global

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   plugins: [react()],
   server: {
     proxy: {
